632a321d196ba5b05ea8b88d85bf409b504853a1
Ohtaka Kaoru <46368397+kkkaoru@users.noreply.github.com>
2024-05-02 18:11:57 +0900

    add a ci (#13)
    * remove a .DS_Store
    
    * add a vrt workflow
    
    * add steps for storycap

--- packages/ui/regconfig.js
+++ packages/ui/regconfig.js
@@ -1,15 +1,5 @@
 //@ts-check
 
-/** @type import('reg-publish-s3-plugin/lib/s3-publisher-plugin').PluginConfig */
-const regPublishS3Plugin = {
-	bucketName: process.env.AWS_S3_BUCKET_NAME || '',
-	customDomain: process.env.AWS_S3_CUSTOM_DOMAIN,
-	sdkOptions: {
-		region: 'auto',
-		endpoint: process.env.AWS_END_POINT,
-	},
-};
-
 /** @type import('reg-suit-interface').RegSuitConfiguration */
 export default {
 	core: {
@@ -18,6 +8,18 @@ export default {
 	},
 	plugins: {
 		'reg-keygen-git-hash-plugin': true,
-		'reg-publish-s3-plugin': regPublishS3Plugin,
+		/** @type import('reg-notify-github-plugin/lib/github-notifier-plugin').GitHubPluginOption */
+		'reg-notify-github-plugin': {
+			clientId: process.env.REG_SUIT_GITHUB_CLIENT_ID,
+		},
+		/** @type import('reg-publish-s3-plugin/lib/s3-publisher-plugin').PluginConfig */
+		'reg-publish-s3-plugin': {
+			bucketName: process.env.AWS_S3_BUCKET_NAME || '',
+			customDomain: process.env.AWS_S3_CUSTOM_DOMAIN,
+			sdkOptions: {
+				region: 'auto',
+				endpoint: process.env.AWS_END_POINT,
+			},
+		},
 	},
 };
